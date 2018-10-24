634b0ba1738ff3e0434723fc6ab51778f9aac465
mart966b <31879834+mart966b@users.noreply.github.com>
2018-10-24 10:11:25 +0200

    Add project files.

---
+++ ClassLibHighscore/Highscore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace ClassLibHighscore
+{
+    public class Highscore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Highscore(int score, string name)
+        {
+            Score = score;
+            Name = name;
+        }
+
+    }
+}
---
+++ RestAPI/Controllers/HighscoreController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibHighscore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HighscoreController : ControllerBase
+    {
+        private static List<Highscore> HScores = new List<Highscore>();
+        // GET: api/Highscore
+        [HttpGet]
+        public List<Highscore> Get()
+        {
+            return HScores;
+        }
+
+        // GET: api/Highscore/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Highscore
+        [HttpPost ("High")]
+        public void Post([FromBody] Highscore Data)
+        {
+            HScores.Add(Data);
+        }
+
+        // PUT: api/Highscore/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
